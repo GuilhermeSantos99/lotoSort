77386415da722643a04a85a4e0ee0d5270fa51ff
Guilherme Santos <guilhermesanctus6@gmail.com>
2022-01-29 17:27:37 -0300

    Added header
    Added header file for better code organization.

--- lotoSort.cpp
+++ lotoSort.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <random>
+#include <algorithm>
+#include "numRandoms.h"
 
 using namespace std;
-
-void quina();
-void sena();
-void mania();
-void facil();
-
 
 int main(){
   
@@ -57,82 +53,4 @@ int main(){
   }
  } 
   return 0;
-}
-
-void quina(){
-
-  using std::cout;
-  using std::endl;
-
-  constexpr int MIN = 1;
-  constexpr int MAX = 80;
-
-  constexpr int RAND_NUMS_TO_GENERATE = 5;
-    
-  std::random_device rd;
-  std::default_random_engine eng(rd());
-  std::uniform_int_distribution<int> distr(MIN, MAX);
-
-  for (int n = 0; n < RAND_NUMS_TO_GENERATE; ++n){
-    cout << distr(eng) << "; ";
-  }
-  cout << endl;
-}
-
-void sena(){
-  using std::cout;
-  using std::endl;
-
-  constexpr int MIN = 1;
-  constexpr int MAX = 60;
-
-  constexpr int RAND_NUMS_TO_GENERATE = 6;
-    
-  std::random_device rd;
-  std::default_random_engine eng(rd());
-  std::uniform_int_distribution<int> distr(MIN, MAX);
-
-  for (int n = 0; n < RAND_NUMS_TO_GENERATE; ++n){
-    cout << distr(eng) << "; ";
-  }
-  cout << endl;
-
-}
-
-void mania(){
-  using std::cout;
-  using std::endl;
-
-  constexpr int MIN = 1;
-  constexpr int MAX = 60;
-
-  constexpr int RAND_NUMS_TO_GENERATE = 6;
-    
-  std::random_device rd;
-  std::default_random_engine eng(rd());
-  std::uniform_int_distribution<int> distr(MIN, MAX);
-
-  for (int n = 0; n < RAND_NUMS_TO_GENERATE; ++n){
-    cout << distr(eng) << "; ";
-  }
-  cout << endl;
-}
-
-void facil(){
-  using std::cout;
-  using std::endl;
-
-  constexpr int MIN = 1;
-  constexpr int MAX = 25;
-
-  constexpr int RAND_NUMS_TO_GENERATE = 15;
-    
-  std::random_device rd;
-  std::default_random_engine eng(rd());
-  std::uniform_int_distribution<int> distr(MIN, MAX);
-
-  for (int n = 0; n < RAND_NUMS_TO_GENERATE; ++n){
-    cout << distr(eng) << "; ";
-  }
-  cout << endl;
 }

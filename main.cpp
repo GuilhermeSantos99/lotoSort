b99bc3b4f5d5423b0694be0ef252693791eb53e2
Guilherme Santos <guilherme.ssilva@protonmail.com>
2022-07-21 23:52:49 -0300

    Correção de bugs e melhorias na apresentação visual

--- main.cpp
+++ main.cpp
@@ -6,51 +6,59 @@
 using namespace std;
 
 int main(){
+
   
  while(true){
+	
+	string tipojog;
 
-   int tipojog;
+	cout << endl;
+  	cout << "======= lotoSort ========" << endl;
+	cout << "_________________________" << endl;
+ 	cout << "| - Jogos disponíveis - |" << endl;
+	cout << "|-----------------------|" << endl;
+	cout << "| 1 - Quina             |" << endl;
+	cout << "| 2 - Mega-Sena         |" << endl;
+	cout << "| 3 - LotoMania         |" << endl;
+	cout << "| 4 - LotoFácil         |" << endl;
+	cout << "|-----------------------|" << endl;
+ 	cout << endl << "Digite o número correspondente ao tipo de aposta: " << endl;
+	cout << ">";
+  	cin >> tipojog;
+  	cout << endl;
 
-   cout << "=== Gerador de números para apostas de loteria ===";
-   cout << endl << endl;
+	if(tipojog == "1")
+	{
+		cout << "Quina" << endl;
+		quina(); 
+	}else if(tipojog == "2")
+	{
+		cout << "Mega-Sena" << endl;
+		sena();
+	}else if(tipojog == "3")
+	{
+		cout << "LotoMania" << endl;
+		mania();
+	}else if(tipojog == "4")
+	{
+		cout << "LotoFácil" << endl;
+		facil();
+	}else
+	{
+		cout << "Opção inválida, tente novamente." << endl;
+		cout << endl;
+	}
 
-   cout << "Jogos disponíveis" << endl;
-   cout << "1 - Quina" << endl << "2 - Mega-Sena" << endl
-     << "3 - LotoMania" << endl << "4 - LotoFácil" << endl;
-   cout << "Digite o número correspondente ao tipo de aposta: ";
-   cin >> tipojog;
-   cout << endl;
+	char varConti;
 
-   if(tipojog == 1){
-     cout << "Quina" << endl;
-     quina(); 
-   }
-   else if(tipojog == 2){
-     cout << "Mega-Sena" << endl;
-     sena();
-   }
-   else if(tipojog == 3){
-     cout << "LotoMania" << endl;
-     mania();
-   }
-   else if(tipojog == 4){
-     cout << "LotoFácil" << endl;
-     facil();
-   }
-   else{
-     cout << "Opção inválida, tente novamente." << endl;
-     cout << endl;
-   }
+	cout << "Deseja gerar mais números aleatórios? [S/N] ";
+	cin >> varConti;
+	tolower(varConti);
 
-  char varConti;
-  cout << endl << "Deseja gerar mais números aleatórios? [S/N] ";
-  cin >> varConti;
-  tolower(varConti);
-
-  if (varConti == 'n')
-  {
-    break;
-  }
+	if (varConti == 'n')
+	{
+		break;
+	}
  } 
   return 0;
 }

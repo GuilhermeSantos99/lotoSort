082daafb49c3f3a7a2c8af509dc4493d43e9dae6
Guilherme Santos <guilherme.ssilva@protonmail.com>
2022-09-17 23:35:40 -0300

    Run function moved to .hpp file (good practices)

--- main.cpp
+++ main.cpp
@@ -5,47 +5,6 @@
 
 int main(){
 
- while(true){
-	
-	std::string tipojog;
-
-	showTips();
-
-	std::cin >> tipojog;
-	std::cout << '\n';
-
-	if(tipojog == "1")
-	{
-		std::cout << "Quina" << '\n';
-		quina(); 
-	}else if(tipojog == "2")
-	{
-		std::cout << "Mega-Sena" << '\n';
-		sena();
-	}else if(tipojog == "3")
-	{
-		std::cout << "LotoMania" << '\n';
-		mania();
-	}else if(tipojog == "4")
-	{
-		std::cout << "LotoFácil" << '\n';
-		facil();
-	}else
-	{
-		std::cout << "Opção inválida, tente novamente." << '\n';
-		std::cout << '\n';
-	}
-
-	char varConti;
-
-	std::cout << "Deseja gerar mais números aleatórios? [S/N] ";
-	std::cin >> varConti;
-	tolower(varConti);
-
-	if (varConti == 'n')
-	{
-		break;
-	}
- } 
+	void seleSort();	
   return 0;
 }

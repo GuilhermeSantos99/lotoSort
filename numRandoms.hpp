e5e99ddabfac0c5b6903a05eabded6fd2b674cfe
Guilherme Santos <guilherme.ssilva@protonmail.com>
2022-09-17 23:32:52 -0300

    Added run function to the header file

--- numRandoms.hpp
+++ numRandoms.hpp
@@ -9,6 +9,7 @@ void sena();
 void mania();
 void facil();
 void showTips();
+void run();
 
 void quina()
 {
@@ -125,6 +126,52 @@ void showTips()
 	std::cout << "|-----------------------|" << '\n';
  	std::cout << '\n' << "Digite o número correspondente ao tipo de aposta: " << '\n';
 	std::cout << "> ";
+}
+
+void run()
+{
+  while(true)
+ {	
+	std::string tipojog;
+
+	showTips();
+
+	std::cin >> tipojog;
+	std::cout << '\n';
+
+	if(tipojog == "1")
+	{
+		std::cout << "Quina" << '\n';
+		quina(); 
+	}else if(tipojog == "2")
+	{
+		std::cout << "Mega-Sena" << '\n';
+		sena();
+	}else if(tipojog == "3")
+	{
+		std::cout << "LotoMania" << '\n';
+		mania();
+	}else if(tipojog == "4")
+	{
+		std::cout << "LotoFácil" << '\n';
+		facil();
+	}else
+	{
+		std::cout << "Opção inválida, tente novamente." << '\n';
+		std::cout << '\n';
+	}
+
+	char varConti;
+
+	std::cout << "Deseja gerar mais números aleatórios? [S/N] ";
+	std::cin >> varConti;
+	tolower(varConti);
+
+	if (varConti == 'n')
+	{
+		break;
+	}
+ } 
 }
 
 #endif

e2ac9919c39f0a6d1c8fc814d83e5892c9511833
Guilherme Santos <guilherme.ssilva@protonmail.com>
2022-10-09 00:01:38 -0300

    Separate functions in own file

--- lotoSort.cpp
+++ lotoSort.cpp
@@ -1,15 +1,55 @@
-#ifndef NUMRANDOMS_HPP_INCLUDED
-#define NUMRANDOMS_HPP_INCLUDED
-
+#include "lotoSort.hpp"
+#include <cstdio>
 #include <iostream>
 #include <random>
+#include <algorithm>
 
-void quina();
-void sena();
-void mania();
-void facil();
-void showTips();
-void run();
+void run()
+{
+  while(true)
+ {	
+	std::string tipojog;
+
+	showTips();
+
+	std::cin >> tipojog;
+	std::cout << '\n';
+
+	if(tipojog == "1")
+	{
+		std::cout << "Quina" << '\n';
+		quina(); 
+	}else if(tipojog == "2")
+	{
+		std::cout << "Mega-Sena" << '\n';
+		sena();
+	}else if(tipojog == "3")
+	{
+		std::cout << "LotoMania" << '\n';
+		mania();
+	}else if(tipojog == "4")
+	{
+		std::cout << "LotoFácil" << '\n';
+		facil();
+	}else
+	{
+		std::cout << "Opção inválida, tente novamente." << '\n';
+		std::cout << '\n';
+	}
+
+	char varConti;
+
+	std::cout << "Deseja gerar mais números aleatórios? [S/N] ";
+	std::cin >> varConti;
+	tolower(varConti);
+
+	if (varConti == 'n')
+	{
+		break;
+	}
+ } 
+}
+
 
 void quina()
 {
@@ -127,51 +167,3 @@ void showTips()
  	std::cout << '\n' << "Digite o número correspondente ao tipo de aposta: " << '\n';
 	std::cout << "> ";
 }
-
-void run()
-{
-  while(true)
- {	
-	std::string tipojog;
-
-	showTips();
-
-	std::cin >> tipojog;
-	std::cout << '\n';
-
-	if(tipojog == "1")
-	{
-		std::cout << "Quina" << '\n';
-		quina(); 
-	}else if(tipojog == "2")
-	{
-		std::cout << "Mega-Sena" << '\n';
-		sena();
-	}else if(tipojog == "3")
-	{
-		std::cout << "LotoMania" << '\n';
-		mania();
-	}else if(tipojog == "4")
-	{
-		std::cout << "LotoFácil" << '\n';
-		facil();
-	}else
-	{
-		std::cout << "Opção inválida, tente novamente." << '\n';
-		std::cout << '\n';
-	}
-
-	char varConti;
-
-	std::cout << "Deseja gerar mais números aleatórios? [S/N] ";
-	std::cin >> varConti;
-	tolower(varConti);
-
-	if (varConti == 'n')
-	{
-		break;
-	}
- } 
-}
-
-#endif
